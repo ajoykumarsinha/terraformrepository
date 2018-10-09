81ab9e72016bfbb5c00d04df4850992b622e4f80
ajoy kumar sinha <ajoykumarsinha@gmail.com>
2018-10-09 18:07:06 +0530

    Second Commit

---
+++ nodes.tf
@@ -0,0 +1,39 @@
+
+
+
+//  Create the two nodes.
+resource "aws_instance" "node1" {
+  ami                  = "${data.aws_ami.amazonlinux.id}"
+  instance_type        = "m4.large"
+  subnet_id            = "${aws_subnet.public-subnet.id}"
+  ##iam_instance_profile = "${aws_iam_instance_profile.openshift-instance-profile.id}"
+  ##user_data            = "${data.template_file.setup-node.rendered}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.openshift-vpc.id}",
+    "${aws_security_group.openshift-public-ingress.id}",
+    "${aws_security_group.openshift-public-egress.id}",
+  ]
+
+  //  We need at least 30GB for OpenShift, let's be greedy...
+  root_block_device {
+    volume_size = 50
+  }
+
+  # Storage for Docker, see:
+  # https://docs.openshift.org/latest/install_config/install/host_preparation.html#configuring-docker-storage
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_size = 80
+  }
+
+  key_name = "${var.key_name}"
+
+  tags {
+    Name    = "${var.name_tag_prefix} Node 1"
+    Project = "openshift"
+    owner = "${var.owner}"
+    TTL = "${var.ttl}"
+  }
+}
+
